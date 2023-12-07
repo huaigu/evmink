5f43960a78b459fb4db582c5c4d37f64726fabd8
Chao wang <wangchao@msn.cn>
2023-12-07 15:31:21 +0800

    opt-remove maxgasfee and to

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,33 +3,33 @@ const axios = require('axios');
 const readline = require('readline');
 
 const BATCH_SIZE = 10;
-const BATCH_TIME = 1;
+const BATCH_TIME = 2;
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-async function mint(to, rpc, private_key, gasPrice, maxFeePerGas, maxPriorityFeePerGas, data) {
+async function mint(rpc, private_key, gasPrice, maxFeePerGas, maxPriorityFeePerGas, data) {
     const web3 = new Web3(rpc);
     const account = web3.eth.accounts.privateKeyToAccount(private_key);
 
     const chain_id = await web3.eth.getChainId();
-    to = web3.utils.toChecksumAddress(to);
+    // to = web3.utils.toChecksumAddress(to);
 
     let nonce = await web3.eth.getTransactionCount(account.address);
     const gasPriceWei = web3.utils.toWei(gasPrice.toString(), 'gwei');
-    const maxFeePerGasWei = web3.utils.toWei(maxFeePerGas.toString(), 'gwei');
+    // const maxFeePerGasWei = web3.utils.toWei(maxFeePerGas.toString(), 'gwei');
     const maxPriorityFeePerGasWei = web3.utils.toWei(maxPriorityFeePerGas.toString(), 'gwei');
 
     const tx = {
         from: account.address,
-        to: to,
+        to: account.address, // mint to self
         nonce: nonce,
         gas: 25024,
         gasPrice: gasPriceWei,
-        maxFeePerGas: maxFeePerGasWei,
-        maxPriorityFeePerGas: maxPriorityFeePerGasWei,
+        // maxFeePerGas: maxFeePerGasWei,
+        maxPriorityFeePerGas: maxPriorityFeePerGasWei, // only set maxPriorityFeePerGas
         chainId: chain_id,
         data: data
     };
@@ -106,7 +106,7 @@ function getInput(prompt, check, error_msg) {
 }
 
 async function main() {
-    const _to = await getInput('输入地址(打到那个号)：', addr => addr.length === 42, '地址长度不对, 请检查后重新输入');
+    // const _to = await getInput('输入地址(打到那个号)：', addr => addr.length === 42, '地址长度不对, 请检查后重新输入');
     const _private_key = await getInput('输入私钥(有gas的小号)：', key => key.length === 64 || (key.startsWith('0x') && key.length === 66), '私钥长度不对, 请检查后重新输入');
 
     const _rpc = await getInput('输入RPC：', rpc => rpc.startsWith('https://'), 'RPC格式不对, https://开头, 请检查后重新输入');
@@ -115,7 +115,7 @@ async function main() {
     let _gasPrice, _maxFeePerGas, _maxPriorityFeePerGas;
     if (_eip1559 === '1') {
         _gasPrice = 0;
-        _maxFeePerGas = await getInput('输入maxFeePerGas：', maxFeePerGas => parseFloat(maxFeePerGas) > 0, 'maxFeePerGas必须大于0, 请检查后重新输入');
+        // _maxFeePerGas = await getInput('输入maxFeePerGas：', maxFeePerGas => parseFloat(maxFeePerGas) > 0, 'maxFeePerGas必须大于0, 请检查后重新输入');
         _maxPriorityFeePerGas = await getInput('输入maxPriorityFeePerGas：', maxPriorityFeePerGas => true, 'maxPriorityFeePerGas必须大于0, 请检查后重新输入');
     } else {
         _gasPrice = await getInput('输入gasPrice：', gasPrice => parseFloat(gasPrice) > 0, 'gasPrice必须大于0, 请检查后重新输入');
@@ -125,7 +125,7 @@ async function main() {
 
     const _data = await getInput('输入data，0x开头的16进制数字:', data => data.length > 0, 'data不能为空, 请检查后重新输入');
 
-    await mint(_to, _rpc, _private_key, _gasPrice, _maxFeePerGas, _maxPriorityFeePerGas, _data);
+    await mint(_rpc, _private_key, _gasPrice, _maxFeePerGas, _maxPriorityFeePerGas, _data);
 }
 
 console.log("Batch Mint Tool");
